f7171f300f333b878e69cb6f3332a855955e981a
Clinton <clintonf@gmail.com>
2021-03-16 16:07:20 -0700

    bugfixes

--- demo.sh
+++ demo.sh
@@ -4,7 +4,8 @@ MESSAGE="Hello HoboCat"
 E_MESSAGE="e_message.txt"
 
 echo "This demo uses the file 'keyfile' as the encryption key source"
+echo "It uses a pre-generated encryption key"
 
-python3.8 dc_encrypt $MESSAGE > $E_MESSAGE
+python3.8 dc_encrypt.py "$MESSAGE" > $E_MESSAGE
 
-python3.8 dc_decrypt $E_MESSAGE
+python3.8 dc_decrypt.py $E_MESSAGE
